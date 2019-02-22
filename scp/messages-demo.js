2d059809f95d36f66872eac454ae108f5a52b893
Thomas <thomas.hawkins09@gmail.com>
2019-02-22 18:45:48 -0500

    added the ability to select different channels and load the messages in them from a JSON obj.

--- scp/messages-demo.js
+++ scp/messages-demo.js
@@ -1,4 +1,17 @@
-var messages = [
+let user = {
+    "name": "Kat",
+    "profile_pic": "https://pbs.twimg.com/profile_images/811351709008220160/pJfRFPnq_400x400.jpg",
+    "full_name" : "Katherine Vicks",
+    "handle" : "@K_Vicks",
+    "phone_number" : "416-382-3820",
+    "city" : "Toronto, ON",
+    "email_address" : "kat@myemail.com",
+    "address" : "34 Leval St, M63 3N2",
+    "description" : "Strong user experience (UX) design skills. Experience in user research and interaction design gained through corporate and nonprofit externships. Completed three externships during summer breaks to gain experience in UX design and information architecture. Excels at managing usability testing for XYZ landing pages in preparation for Web site redesign. Very competent in testing parameters, prepared prototypes, recruited users, facilitated tests and reported results.",
+    "description_extended" : "",
+};
+
+let messages_landing = [
     {
         "user": {
             "name": "Kat",
@@ -41,6 +54,35 @@ var messages = [
     }
 ];
 
+let messages_sports = [
+    {
+        "user": {
+            "name": "Ron",
+            "image": "https://images.pexels.com/photos/1498335/pexels-photo-1498335.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=25"
+        },
+        "message": {
+            "text": "Go leafs!",
+            "date": 1534485670000
+        }
+    }
+];
+
+let messages_shows = [];
+let messages_movies = [];
+let messages_pets = [
+    {
+        "user": {
+            "name": "Betty",
+            "image": "https://images.pexels.com/photos/1498778/pexels-photo-1498778.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=25"
+        },
+        "message": {
+            "text": "I love my stupid cat.",
+            "date": 1537564070000
+        }
+    }
+];
+
+
 function createArticle($message) {
     let $article = $("<article>").addClass("message-row");
     let $image = $("<img>").attr("src" , $message.user.image);
@@ -76,6 +118,37 @@ function submitMessage(message) {
     console.log("Message : " + message);
 }
 
+let user_name = document.getElementById("user_name");
+user_name.innerHTML = user["name"];
+
+let current_channel = messages_landing;
+
+
+let sports_button = document.getElementById("Sports");
+let shows_button = document.getElementById("Shows");
+let movies_button = document.getElementById("Movies");
+let pets_button = document.getElementById("Pets");
+
+let buttons=[sports_button, shows_button, movies_button, pets_button];
+
+
+
+sports_button.addEventListener("click", function() {open_channel(sports_button, messages_sports)});
+shows_button.addEventListener("click", function() {open_channel(shows_button, messages_shows)});
+movies_button.addEventListener("click", function() {open_channel(movies_button, messages_movies)});
+pets_button.addEventListener("click", function() {open_channel(pets_button, messages_pets)});
+
+function open_channel(channel_button, messages) {
+    
+    for (let i = 0; i < buttons.length; i++){
+        buttons[i].style.backgroundColor="#7851A9";
+    }
+    channel_button.style.backgroundColor="red";
+    $("#messages-list").empty();
+    renderMessages(messages);
+    current_channel = messages;
+}
+
 
 
 $(document).ready(function() {
@@ -85,22 +158,23 @@ $(document).ready(function() {
             event.preventDefault();
             let message = {
                 "user": {
-                "name": "Ramses",
-                "image": "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=25"
+                    "name": user["name"],
+                    "image": user["profile_pic"]
                 },
                 "message": {
-                "text": event.target.value,
-                "date": (new Date()).getTime()
+                    "text": event.target.value,
+                    "date": (new Date()).getTime()
                 }
             };		
             let $article = createArticle(message);
-            $("#messages-list").prepend($article);	
+            $("#messages-list").prepend($article);
+            current_channel.prepend($article);	
             event.target.value = "";
             console.log(message);			
             submitMessage(message);
         }
         });				
 
-    renderMessages(messages);
+    renderMessages(messages_landing);
 
 });
